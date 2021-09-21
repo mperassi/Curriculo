0b8c2614216f3c899c32b0e265d193ecd5530234
Carolina Ortega <cahortega@outlook.com>
2021-09-21 13:40:38 -0300

    Dados pessoais
    Criação da primeira página

---
+++ Curriculo/DataTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  DateTableViewCell.swift
+//  Curriculo
+//
+//  Created by Carolina Ortega on 20/09/21.
+//
+
+import UIKit
+
+class DataTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var dataObs: UILabel!
+    @IBOutlet weak var dataPicker: UIDatePicker!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Curriculo/TextTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TextTableViewCell.swift
+//  Curriculo
+//
+//  Created by Carolina Ortega on 20/09/21.
+//
+
+import UIKit
+
+class TextoTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nomeLabel: UILabel!
+    @IBOutlet weak var nomeField: UITextField!
+    @IBOutlet weak var nomeObs: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
