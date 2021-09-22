e1ecb12e1fc5960fffa01728047365dec971435b
Carolina Ortega <cahortega@outlook.com>
2021-09-21 21:38:32 -0300

    Alterações dia 21/set
    Últimas alterações feitas no dia 21/set

--- Curriculo/Páginas/FirstPageViewController.swift
+++ Curriculo/Páginas/FirstPageViewController.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import NotificationCenter
 
 class FirstPageViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addLink: UIButton!
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    // Create Date
+    let date = Date()
+
+    // Create Date Formatter
     let dateFormatter = DateFormatter()
     
     let defaults = UserDefaults.standard
@@ -18,6 +25,7 @@ class FirstPageViewController: UIViewController {
 //    let dictValue = UserDefaults.standard.value(forKey: "DictValue") //Retrieving the value from user default
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         self.tableView.dataSource = self
         self.tableView.delegate = self
@@ -25,21 +33,68 @@ class FirstPageViewController: UIViewController {
 //        self.dictionary = defaults.value(forKey: "DictValue") as! [String : String] //colocar nas outras vc, no viewdidload
         defaults.setValue(dictionary, forKey: "DictValue") //Saved the Dictionary in user default (colocar na troca de pag)
         
-        addLink.addTarget(self, action: #selector(acaoLink), for: .touchDown)
+        NotificationCenter.default.addObserver(self, selector: #selector(FirstPageViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+           
+           NotificationCenter.default.addObserver(self, selector: #selector(FirstPageViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
 
+        addLink.addTarget(self, action: #selector(dicionario1Pagina), for: .touchDown)
+
+
+        
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func acaoLink(){
-        print(dictionary)
-//        self.dictionary["Nome"] =
-//        self.dictionary["DataNasc"] =
-//        self.dictionary["Tel"] =
-//        self.dictionary["Local"] =
-//        self.dictionary["Email"] =
-//        self.dictionary["Link"] = 
+    @IBAction func dicionario1Pagina(){
+        tableView.dequeueReusableCell(withIdentifier: "textoCell")
+        let indexPath = NSIndexPath(row: 0, section: 0)
+        let multiCell = tableView.cellForRow(at: indexPath as IndexPath) as? TextoTableViewCell
+        self.dictionary["Nome"] = multiCell?.nomeField.text
+        
+        let indexPathData = NSIndexPath(row: 1, section: 0)
+        let multiCell1 = tableView.cellForRow(at: indexPathData as IndexPath) as? DataTableViewCell
+        dateFormatter.dateFormat = "dd/MM/YY"
+        self.dictionary["Data"] = multiCell1?.dataPicker.date.description
 
         
+        let indexPathTel = NSIndexPath(row: 2, section: 0)
+        let multiCell2 = tableView.cellForRow(at: indexPathTel as IndexPath) as? TextoTableViewCell
+        self.dictionary["Tel"] = multiCell2?.nomeField.text
+        
+        let indexPathLocal = NSIndexPath(row: 3, section: 0)
+        let multiCell3 = tableView.cellForRow(at: indexPathLocal as IndexPath) as? TextoTableViewCell
+        self.dictionary["Local"] = multiCell3?.nomeField.text
+        
+        let indexPathEmail = NSIndexPath(row: 4, section: 0)
+        let multiCell4 = tableView.cellForRow(at: indexPathEmail as IndexPath) as? TextoTableViewCell
+        self.dictionary["Email"] = multiCell4?.nomeField.text
+        
+        let indexPathLink = NSIndexPath(row: 5, section: 0)
+        let multiCell5 = tableView.cellForRow(at: indexPathLink as IndexPath) as? TextoTableViewCell
+        self.dictionary["Link"] = multiCell5?.nomeField.text
+        
+        print(dictionary)
+
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+      guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+      else {
+        // if keyboard size is not available for some reason, dont do anything
+        return
+      }
+
+      let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height , right: 0.0)
+      scrollView.contentInset = contentInsets
+      scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+      let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+          
+      
+      // reset back the content inset to zero after keyboard is gone
+      scrollView.contentInset = contentInsets
+      scrollView.scrollIndicatorInsets = contentInsets
     }
     
 }
@@ -57,60 +112,48 @@ extension FirstPageViewController: UITableViewDataSource{
     func tableView(_ tableview: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
             let cell  = tableView.dequeueReusableCell(withIdentifier: "textoCell", for: indexPath) as! TextoTableViewCell
-            let tipo = "Nome"
-            cell.nomeLabel.text = tipo
+            cell.nomeLabel.text = "Nome"
             cell.nomeField.keyboardType = .default
             cell.nomeField.placeholder = "Digite seu nome completo"
             cell.nomeObs.text = "*Obrigatório"
-            self.dictionary[tipo] = cell.nomeField.text
 //            print(dictValue)  // Printing the value
             return cell
             
         } else if indexPath.row == 1 {
-            let tipo = "DataNasc"
             let cell = tableView.dequeueReusableCell(withIdentifier: "dataCell", for: indexPath) as! DataTableViewCell
-            dateFormatter.dateFormat = "dd/MM/YY"
-            self.dictionary[tipo] = dateFormatter.string(from: cell.dataPicker.date)
+
             return cell
             
         } else if indexPath.row == 2 {
-            let tipo = "Tel"
             let cell  = tableView.dequeueReusableCell(withIdentifier: "textoCell", for: indexPath) as! TextoTableViewCell
             cell.nomeLabel.text = "Telefone"
             cell.nomeField.placeholder = "Exemplo: (00) 00000-0000"
             cell.nomeField.keyboardType = .numberPad
             cell.nomeObs.text = "*Obrigatório"
-            self.dictionary[tipo] = cell.nomeField.text
             return cell
             
         } else if indexPath.row == 3 {
-            let tipo = "Local"
             let cell  = tableView.dequeueReusableCell(withIdentifier: "textoCell", for: indexPath) as! TextoTableViewCell
             cell.nomeLabel.text = "Localidade"
             cell.nomeField.keyboardType = .default
             cell.nomeField.placeholder = "Exemplo: São Paulo - SP"
             cell.nomeObs.text = "*Obrigatório"
-            self.dictionary[tipo] = cell.nomeField.text
             return cell
             
         } else if indexPath.row == 4 {
-            let tipo = "Email"
             let cell  = tableView.dequeueReusableCell(withIdentifier: "textoCell", for: indexPath) as! TextoTableViewCell
             cell.nomeLabel.text = "E-mail"
             cell.nomeField.keyboardType = .emailAddress
             cell.nomeField.placeholder = "Exemplo: email@exemplo.com"
             cell.nomeObs.text = "*Obrigatório"
-            self.dictionary[tipo] = cell.nomeField.text
             return cell
             
         } else {
-            let tipo = "Link"
             let cell  = tableView.dequeueReusableCell(withIdentifier: "textoCell", for: indexPath) as! TextoTableViewCell
             cell.nomeLabel.text = "Links adicionais"
             cell.nomeField.placeholder = "Exemplo: LinkedIN, Behance, Github"
             cell.nomeField.keyboardType = .URL
             cell.nomeObs.text = " "
-            self.dictionary[tipo] = cell.nomeField.text
             return cell
             
         }
