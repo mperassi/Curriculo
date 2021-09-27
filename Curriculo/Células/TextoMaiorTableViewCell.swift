b116bba92a5396f0f4b17fa6387db9e7204837d4
Carolina Ortega <cahortega@outlook.com>
2021-09-27 13:46:17 -0300

    27/09 atualizações Carol
    grupinho :)

--- Curriculo/Células/TextoMaiorTableViewCell.swift
+++ Curriculo/Células/TextoMaiorTableViewCell.swift
@@ -17,6 +17,8 @@ class TextoMaiorTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
         largeTextView?.layer.cornerRadius = 10
+        largeTextView?.delegate = self
+        
         
     }
 
@@ -27,4 +29,13 @@ class TextoMaiorTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+
+}
+extension TextoMaiorTableViewCell: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            let currentText = textView.text ?? ""
+            guard let stringRange = Range(range, in: currentText) else { return false }
+            let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
+            return updatedText.count <= 300 // Change limit based on your requirement.
+        }
 }
